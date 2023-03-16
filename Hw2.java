426d3847cf5d73068e3fa33d66ddd3ff64457b79
Artur Puchkov <artur.puchkov@russianpost.ru>
2023-03-16 21:29:42 +0300

    done hw2

---
+++ Hw2.java
@@ -0,0 +1,12 @@
+public class Hw2 {
+    public static void main(String[] args) {
+        int a = 33;
+        int b = 4;
+
+        System.out.println("Результат сложения a и b = " + (a + b));
+        System.out.println("Результат вычитания a и b = " + (a - b));
+        System.out.println("Результат деления a на b = " + (a/b));
+
+
+    }
+}
