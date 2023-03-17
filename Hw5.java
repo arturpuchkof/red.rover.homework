5491ece37e0403d08fd121937ed45fa85c1b9e58
Artur Puchkov <artur.puchkov@russianpost.ru>
2023-03-17 20:51:11 +0300

    done hw5

--- Hw5.java
+++ Hw5.java
@@ -1,2 +1,45 @@
 public class Hw5 {
-}
+    public static void main(String[] args) {
+
+       int rows = 9;
+
+        for (int i = rows; i >= 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("==============================>");
+
+        for (int i = rows; i >= 0 ; --i) {
+            for (int j = rows; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i ; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("==============================>");
+
+        for (int i = rows; i >= 0 ; --i) {
+            for (int j = rows; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 0 ; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        }
+
+    }
+
+
+
+
