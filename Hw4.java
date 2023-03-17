e5f3bfb7997dbd05495dab681a820ac0eb4334e0
Artur Puchkov <artur.puchkov@russianpost.ru>
2023-03-17 15:11:05 +0300

    done hw4

--- Hw4.java
+++ Hw4.java
@@ -1,2 +1,25 @@
 public class Hw4 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++){
+            System.out.println(i);
+        }
+        System.out.println("===========================>");
+
+        for (int i = 5; i < 10000; i = i*5) {
+            System.out.println(i);
+        }
+        System.out.println("===========================>");
+
+        for (int i = 40; i < 61; i++) {
+            if (i % 4 == 0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("===========================>");
+
+        for (int i = 40; i < 61; i = i +4) {
+            System.out.println(i);
+        }
+
+    }
 }
